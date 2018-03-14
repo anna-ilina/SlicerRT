bbd2882c7e3905f911f2ff65862e2df93a1f21b5
Anna Ilina <ilina.anna@queensu.ca>
2018-03-14 18:40:02 -0400

    ENH: Ctcreate using orthovoltage dose engine working

--- ExternalBeamPlanning/DoseEngines/Python/OrthovoltageDoseEngine.py
+++ ExternalBeamPlanning/DoseEngines/Python/OrthovoltageDoseEngine.py
@@ -12,19 +12,42 @@ class OrthovoltageDoseEngine(AbstractScriptedDoseEngine):
     AbstractScriptedDoseEngine.__init__(self, scriptedEngine)
 
   def defineBeamParameters(self):
+    ##########################################
+    # Generate ctcrate phantom parameters tab
+    ##########################################
+
     self.scriptedEngine.addBeamParameterLineEdit(
     "Generate ctcreate phantom", "CtcreateOutputPath", "Ctcreate output filepath:", 
-    "Enter filepath to store ctcreate phantom and related files", "") 
+    "Enter filepath to store ctcreate phantom and related files", "C:\\\\d\\\\tmp") 
 
     self.scriptedEngine.addBeamParameterLineEdit(
     "Generate ctcreate phantom", "SeriesIUD", "Series IUD:", 
-    "Enter the series IUD of the CT phantom being used (to get DICOM slicenames for ctcreate)", "") 
+    "Enter the series IUD of the CT phantom being used (to get DICOM slicenames for ctcreate)", 
+    "1.2.840.113704.1.111.3228.1498828556.10")
 
     self.scriptedEngine.addBeamParameterLineEdit(
-    "Generate ctcreate phantom", "ROIName", "ROI name for cropping image (optional):", 
-    "Enter name of ROI, if you wish to crop CT image which will be converted to ctcreate phantom format", "") 
+    "Generate ctcreate phantom", "ROIName", "ROI node name for cropping image (optional):", 
+    "Enter name of ROI, if you wish to crop CT image which will be converted to ctcreate phantom format. \
+    If no ROI entered, will use image bounds from original CT image volume", "") 
 
-    #Todo: add ROI selection box 
+    self.scriptedEngine.addBeamParameterLineEdit(
+    "Generate ctcreate phantom", "SliceThicknessX", "Slice thickness (mm) in X direction (optional):", 
+    "Enter desired slice thickness in X direction for output ctcreate phantom. If not x,y,z slice \
+    thickness not provided, will use same thickness from original CT image volume", "") 
+
+    self.scriptedEngine.addBeamParameterLineEdit(
+    "Generate ctcreate phantom", "SliceThicknessY", "Slice thickness (mm) in Y direction (optional):", 
+    "Enter desired slice thickness in Y direction for output ctcreate phantom. If not x,y,z slice \
+    thickness not provided, will use same thickness from original CT image volume", "") 
+
+    self.scriptedEngine.addBeamParameterLineEdit(
+    "Generate ctcreate phantom", "SliceThicknessZ", "Slice thickness (mm) in Z direction (optional):", 
+    "Enter desired slice thickness in Z direction for output ctcreate phantom. If not x,y,z slice \
+    thickness not provided, will use same thickness from original CT image volume", "") 
+
+    ##########################################
+    # Orthovoltage dose parameters tab
+    ##########################################
 
     self.scriptedEngine.addBeamParameterLineEdit(
     "Orthovoltage dose", "SimulationTitle", "Simulation title:", 
@@ -51,32 +74,56 @@ class OrthovoltageDoseEngine(AbstractScriptedDoseEngine):
     Use with caution.", "100")
 
 
-
-  #todo: verify that all entered are valid
+  #todo: verify that all entered parameters are valid
   def calculateDoseUsingEngine(self, beamNode, resultDoseVolumeNode):
     import qSlicerExternalBeamPlanningDoseEnginesPythonQt as engines
     orthovoltageEngine = engines.qSlicerMockDoseEngine()
 
-    # get ctcreate parameters
+    ##########################################
+    # Get ctcreate parameters
+    ##########################################
+
     ctcreateOutputPath = self.scriptedEngine.parameter(beamNode, "CtcreateOutputPath")
     seriesIUD = self.scriptedEngine.parameter(beamNode, "SeriesIUD")
     ROIName = self.scriptedEngine.parameter(beamNode, "ROIName")
-    #todo: allow user to input voxel thickness (optional)
+    volumeName = self.scriptedEngine.parameter(beamNode, "VolumeName")
+    sliceThicknessX = self.scriptedEngine.parameter(beamNode, "SliceThicknessX")
+    sliceThicknessY = self.scriptedEngine.parameter(beamNode, "SliceThicknessY")
+    sliceThicknessZ = self.scriptedEngine.parameter(beamNode, "SliceThicknessZ")
+
+    parentPlan = beamNode.GetParentPlanNode()
+    volumeNode = parentPlan.GetReferenceVolumeNode()
 
     if ROIName == "":
       ROI = None
     else:
       try:
-        ROI = slicer.mrmlScene.GetNodeByID(ROIName)
+        ROI = slicer.util.getNode(ROIName)
       except:
         print("Unable to get specified ROI, check for typo")  #todo: this should be output to the Warnings log
         ROI = None
-  
 
-    v = None  #todo: get volume node from parent plan (needed as input to determine thickness)
+    if sliceThicknessX == "" or sliceThicknessY == "" or sliceThicknessZ == "":
+      thicknesses = None
+    else:
+      thicknesses = [float(sliceThicknessX), float(sliceThicknessY), float(sliceThicknessZ)]
 
+    ##########################################
     # Call ctcreate
-    generateCtcreateInput(v,seriesIUD, ctCreateOutputPath, ROI)
+    ##########################################
+
+    generateCtcreateInput(volumeNode,seriesIUD, ctcreateOutputPath, ROI, thicknesses)
+    callCtcreate(ctcreateOutputPath)
+
+    ##########################################
+    # Get DOSXYZnrc parameters
+    ##########################################
+
+
+    ##########################################
+    # Call DOSXYZnrc
+    ##########################################
+
 
     #Call C++ orthovoltage engine to calculate orthovoltage dose
     return orthovoltageEngine.calculateDoseUsingEngine(beamNode, resultDoseVolumeNode)
